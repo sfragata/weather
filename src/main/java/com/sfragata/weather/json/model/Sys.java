129d78868e1bea799531b7e659fce42844f5fb06
sfragata <silviofragata@yahoo.com.br>
2014-02-10 20:00:41 -0500

    Add ignore property if unknow

--- src/main/java/com/sfragata/weather/json/model/Sys.java
+++ src/main/java/com/sfragata/weather/json/model/Sys.java
@@ -5,6 +5,8 @@ package com.sfragata.weather.json.model;
 
 import java.io.Serializable;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 /**
  * @author frasi1
  * 
@@ -12,6 +14,7 @@ import java.io.Serializable;
  * 
  *         "sys":{"country":"CA","sunrise":1371113396,"sunset":1371170416}
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Sys implements Serializable {
 
 	private static final long serialVersionUID = -1519929139285862193L;
